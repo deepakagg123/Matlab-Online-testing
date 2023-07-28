4c536824ce668c3f9534fb48b854bf38f475c993
Deepak Aggarwal <deepakagg@ipr.res.in>
2023-07-28 14:25:43 +0530

    matrix multiplication file added

---
+++ matrix_multiplication.m
@@ -0,0 +1,8 @@
+n = 5000;                     % set matrix size
+A = rand(n);                % create random matrix
+B = rand(n);                % create another random matrix
+tic                                % calculate the elapsed time using tic and toc
+C = A * B;                  % matrix multiplication
+time=toc;                    % calculate the elapsed time using tic and toc
+fprintf(['Processing time: ' num2str(time)]); % display running time (unit: second)
+save('C.mat', 'C')
